fc650421d4f1dffbc23156284ad12ed029ed3802
lsh17831048 <ars8129@naver.com>
2022-04-27 23:35:20 +0900

    Oracle JDBC Driver 추가 / 경로-(webapp/WEB-INF/lib) 회원가입, 로그인페이지 이동 Controller 추가

---
+++ src/main/java/com/healingPill/MemberController/MemberController.java
@@ -0,0 +1,26 @@
+package com.healingPill.MemberController;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/memberJoin")
+public class MemberController {
+    // 로그 기록을 남기기 위한 Logger 클래스
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+    // 회원가입 페이지 이동
+    @RequestMapping(value = "memberJoin", method = RequestMethod.GET)
+    public void memberJoinGET() {
+        logger.debug("회원가입 페이지 이동");
+    }
+    
+    // 로그인 페이지 이동
+    @RequestMapping(value = "memberLogin", method = RequestMethod.GET)
+    public void memberLoginGET() {
+        logger.debug("로그인 페이지 이동");
+    }
+}
