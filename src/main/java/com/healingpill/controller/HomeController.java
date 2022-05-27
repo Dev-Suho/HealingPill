213a421097f02479c0c832983f6b925957ce3cc4
정유진 <junggg097@daum.net>
2022-05-27 09:33:48 +0900

    관리자 css, js 추가 / 관리자 상품추가 , 회원관리 , 매거진 추가 FRONTEND

--- src/main/java/com/healingpill/controller/HomeController.java
+++ src/main/java/com/healingpill/controller/HomeController.java
@@ -10,4 +10,15 @@ public class HomeController {
     public String main() {
         return "index";
     }
+
+    //관리자 페이지
+    //상품추가
+    @RequestMapping(value = "/admin/product_add")
+    public String pdadd() { return "/admin/product_add";}
+    //관리자 첫 페이지
+    @RequestMapping(value = "/admin/index")
+    public String adminindex() { return "/admin/index";}
+    //매거진 추가
+    @RequestMapping(value = "/admin/cm_magazine")
+    public String adminmagazine() { return "/admin/cm_magazine";}
 }
