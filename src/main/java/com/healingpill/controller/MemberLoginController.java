281f4841788c5bc75845e2b0874def7079880182
정유진 <junggg097@daum.net>
2022-11-21 11:29:39 +0900

    관리자 로그인 시 현재 창 페이지 대체

--- src/main/java/com/healingpill/controller/MemberLoginController.java
+++ src/main/java/com/healingpill/controller/MemberLoginController.java
@@ -55,7 +55,7 @@ public class MemberLoginController {
             session.setAttribute("member", res);
             response.setContentType("text/html; charset=UTF-8");
             PrintWriter out = response.getWriter();
-            out.println("<script>alert('메인 관리자 권한으로 로그인 되었습니다.');window.open('/admin2/customer','_blanck');</script>");
+            out.println("<script>alert('메인 관리자 권한으로 로그인 되었습니다.');window.open('/admin2/customer','_self');</script>");
             out.flush();
 
 
